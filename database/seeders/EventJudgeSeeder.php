ace9a33f4d0f9f7809962704ce0be3981f25a954
Zaid Díaz <82064141+DiCZDC@users.noreply.github.com>
2025-12-07 13:00:08 -0600

    created pdf generator

--- database/seeders/EventJudgeSeeder.php
+++ database/seeders/EventJudgeSeeder.php
@@ -4,14 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\EventUser;
-class EventUserSeeder extends Seeder
+
+class EventJudgeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        EventUser::factory(50)->create();
+        //
     }
 }
